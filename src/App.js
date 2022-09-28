b2856fc21ccf1d3a17f27d97d44d0100e127d5df
Pedro <pedrosabino@outlook.com>
2022-09-27 21:49:36 -0300

    Criação do projeto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import styled from "styled-components"
+// import GlobalStyle from "./GlobalStyle";
+
+export default function App() {
+    return (
+        <>
+            <span>Olá mundo!</span>
+            {/* <GlobalStyle/> */}
+        </>
+    )
+}
